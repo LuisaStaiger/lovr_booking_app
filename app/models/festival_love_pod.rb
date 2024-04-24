513114848eda77da51d21ae416a0c635cbb940a9
Louise Giraud <louisegiraud7@gmail.com>
2024-04-24 15:54:10 +0200

    new migrations and seeds

--- app/models/festival_love_pod.rb
+++ app/models/festival_love_pod.rb
@@ -1,5 +1,4 @@
 class FestivalLovePod < ApplicationRecord
   belongs_to :festival
   belongs_to :love_pod
-  has_many :bookings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_23_110251) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_24_131619) do
   create_table "bookings", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "love_pod_id", null: false
@@ -20,8 +20,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_23_110251) do
     t.datetime "start_time"
     t.datetime "reservation_start"
     t.integer "duration"
-    t.datetime "temporary"
     t.integer "festival_id", null: false
+    t.integer "status", default: 0
     t.index ["festival_id"], name: "index_bookings_on_festival_id"
     t.index ["love_pod_id"], name: "index_bookings_on_love_pod_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
@@ -61,7 +61,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_23_110251) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
