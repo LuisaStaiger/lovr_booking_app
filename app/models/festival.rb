1a6f086e278f3de4df5a3a6e1ddb7484157dee23
Louise Giraud <louisegiraud7@gmail.com>
2024-05-22 15:54:38 +0200

    new colors, font, new index style and attached festival images

--- app/models/festival.rb
+++ app/models/festival.rb
@@ -3,6 +3,7 @@ class Festival < ApplicationRecord
   has_many :love_pods, through: :festival_love_pods
   has_many :available_slots, dependent: :destroy
   has_many :bookings, through: :available_slots, dependent: :destroy
+  has_one_attached :photo
 
   validates :name ,:location, :start_date, :end_date, presence: true
 end
