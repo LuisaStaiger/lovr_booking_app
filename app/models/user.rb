2ad84b1a40163a108e35fefd1b33648924f5a53a
Luisa Staiger <staigerluisa@gmail.com>
2024-05-25 10:01:22 +0200

    Uncommented admin? method in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # def admin?
-  #   admin
-  # end
+  def admin?
+    admin
+  end
 end
