518976323159b51dc78aa2cc456f36d324f51d15
Luisa Staiger <staigerluisa@gmail.com>
2024-05-25 13:17:57 +0200

    changes for heroku deployment

--- db/migrate/20240424131515_add_admin_to_users.rb
+++ db/migrate/20240424131515_add_admin_to_users.rb
@@ -1,6 +1,6 @@
 class AddAdminToUsers < ActiveRecord::Migration[7.1]
   def change
-    unless ActiveRecord::Base.connection.column_exists?(:users, :admin)
+    unless column_exists? :users, :admin
       add_column :users, :admin, :boolean, default: false
     end
   end
