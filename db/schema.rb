9a9e7bd0bde3e169760174818c5045bc251a0b26
Louise Giraud <louisegiraud7@gmail.com>
2024-05-28 11:17:14 +0200

    fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_27_113602) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_27_122641) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -60,7 +60,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_27_113602) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "duration"
-    t.integer "status", default: 0
     t.datetime "start_time"
     t.integer "available_slot_id"
     t.string "slots_sku"
