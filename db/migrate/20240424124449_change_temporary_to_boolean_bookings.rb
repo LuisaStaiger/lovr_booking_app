8557f967668ab0b486d9de2e81ad806c09e4ade5
Louise Giraud <louisegiraud7@gmail.com>
2024-05-31 12:21:16 +0200

    temporary

--- db/migrate/20240424124449_change_temporary_to_boolean_bookings.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTemporaryToBooleanBookings < ActiveRecord::Migration[7.1]
-  def up
-    remove_column :bookings, :temporary, :boolean
-  end
-
-  def down
-    add_column :bookings, :temporary, :boolean
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -59,8 +59,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_123343) do
     t.date "booking_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "duration"
     t.datetime "start_time"
+    t.integer "duration"
     t.integer "available_slot_id"
     t.string "slots_sku"
     t.string "checkout_session_id"
