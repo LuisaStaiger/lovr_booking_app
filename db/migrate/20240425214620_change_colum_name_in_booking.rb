7dbc1e9ab9cd248324d59200cdc946392f426f36
Louise Giraud <louisegiraud7@gmail.com>
2024-05-31 12:01:30 +0200

    remove time_frame, reservation_start and number_of_people

--- db/migrate/20240425214620_change_colum_name_in_booking.rb
+++
@@ -1,7 +0,0 @@
-class ChangeColumNameInBooking < ActiveRecord::Migration[7.1]
-  def change
-    # remove_column :bookings, :start_time, :datetime
-    remove_column :bookings, :reservation_start, :datetime
-    add_column :bookings, :time_frame, :datetime
-  end
-end
