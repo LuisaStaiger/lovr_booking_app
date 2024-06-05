7a3650255140b7a6a3edd90a14d032f2903dfe68
Louise Giraud <louisegiraud7@gmail.com>
2024-06-05 18:50:47 +0200

    start_time in schema is back

--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_123343) do
     t.date "booking_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "start_time"
     t.integer "duration"
     t.integer "available_slot_id"
     t.string "slots_sku"
