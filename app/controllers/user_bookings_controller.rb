ba3e90580d7509048af4e6777a739f57113948cc
Louise Giraud <louisegiraud7@gmail.com>
2024-06-06 18:37:39 +0200

    pending booking has a countdown and gets deleted after 30 mins

--- app/controllers/user_bookings_controller.rb
+++ app/controllers/user_bookings_controller.rb
@@ -1,12 +1,25 @@
 class UserBookingsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_bookings, only: [:index, :expire]
 
   def index
-    @bookings = current_user.bookings
+    expire
     @confirmed_bookings = @bookings.where(status: 1)
     @pending_bookings = @bookings.where(status: 0)
   end
 
   def destroy
+  end
+
+  def expire
+    @bookings.each do |booking|
+      if booking.status == "pending" && booking.created_at + 30.minutes <= Time.current
+        booking.destroy
+      end
+    end
+  end
+
+  private
+  def set_bookings
+    @bookings = current_user.bookings
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,4 @@ Rails.application.routes.draw do
   resources :bookings, only: [:destroy]
 
   mount StripeEvent::Engine, at: '/stripe-webhooks'
-
 end
