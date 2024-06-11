12bb3e68bb8cb35c47817e8c94186a9b4b20cc76
Louise Giraud <louisegiraud7@gmail.com>
2024-06-11 18:32:01 +0200

    in progress

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.hosts << "240d-94-223-172-34.ngrok-free.app"
+  config.hosts << "0a7c-94-223-172-34.ngrok-free.app"
 end
